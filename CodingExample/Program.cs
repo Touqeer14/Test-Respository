5c538f8b185b72c4f189b00d81a7da3cd50700ee
touqeer <touqeer.ahmed@ovrlod.com>
2021-01-14 18:00:52 +0500

    TestRepository

---
+++ CodingExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExample
+{
+    class Program
+    {
+         
+            static void Main(string[] args)
+            {
+                childclass ch = new childclass();
+                childclass ch1 = new childclass("Hello ");
+                Console.ReadKey();
+            }
+        }
+
+        class baseclass
+        {
+            public baseclass()
+            {
+                Console.WriteLine("I am Default Constructors");
+            }
+            public baseclass(string message)
+            {
+                Console.WriteLine("Constructor Message : " + message);
+            }
+        }
+
+        class childclass : baseclass
+        {
+            public childclass()
+            {
+            }
+            public childclass(string message) : base(message)
+                
+            {
+            }
+        }
+    }
